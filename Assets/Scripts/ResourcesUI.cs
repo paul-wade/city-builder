c5d20571455b912bcc376f2cadc56d6f8d60e35b
Paul Wade <paul.wade@live.com>
2020-12-26 16:24:06 -0600

    add textmeshpro

---
+++ Assets/Scripts/ResourcesUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using TMPro;
+
+public class ResourcesUI : MonoBehaviour
+{
+    /// <summary>
+    /// The UI template
+    /// </summary>
+    [SerializeField]private Transform resourceTemplate;
+
+    private void Awake()
+    {
+        float offsetAmount = 160f;
+         
+        var _resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name).list;
+        resourceTemplate.gameObject.SetActive(false);
+       var index = 0;
+        foreach (var resourceType in _resourceTypeList)
+        {
+            var resourceTransform = Instantiate(resourceTemplate,transform);
+            resourceTransform.gameObject.SetActive(true);
+            resourceTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(0,offsetAmount * index);
+            
+            var image = resourceTransform.Find("image").GetComponent<Image>();
+            image.sprite = resourceType.Image;
+
+           // var text = resourceTransform.Find("text").GetComponent<TextMeshProGUI>
+            index++;
+        }
+    }
+}
