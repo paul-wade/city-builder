70c3d37b7e3ad72d2d650e7b9ccff5b788c535c1
Paul Wade <paul.wade@questarai.com>
2021-01-16 19:11:51 -0600

    adding building selection UI

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,43 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera cineMachingVirtualCamera;
+
+    private float orthographicSize;
+    private float targetOrthographicSize;
+
+    private void Start()
+    {
+        orthographicSize = cineMachingVirtualCamera.m_Lens.OrthographicSize;
+        targetOrthographicSize = orthographicSize;
+    }
+    private void Update()
+    {
+        HandleMovement();
+        HandleZoom();
+    }
+
+    private void HandleMovement()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        var moveDirection = new Vector3(x, y).normalized;
+        float moveSpeed = 50f;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandleZoom()
+    {
+        targetOrthographicSize += -Input.mouseScrollDelta.y;
+        float minOrthographicSize = 10;
+        float maxOrthographicSize = 30;
+        targetOrthographicSize = Mathf.Clamp(targetOrthographicSize, minOrthographicSize, maxOrthographicSize);
+        float zoomSpeed = 5f;
+        orthographicSize = Mathf.Lerp(orthographicSize, targetOrthographicSize, Time.deltaTime * zoomSpeed);
+        cineMachingVirtualCamera.m_Lens.OrthographicSize = orthographicSize;
+    }
+}
